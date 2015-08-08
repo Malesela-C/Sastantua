885b239420fb09c51c25b32d8765735675330d69
Elliot BITSCH <ebitsch@e1r3p9.42.fr>
2015-08-08 12:08:54 +0200

    small fix

---
+++ src/sastantua2.py
@@ -0,0 +1,50 @@
+import sys
+size = int(sys.argv[1])
+
+#Height fron size
+heights = []
+sup_stars = [0,4,4]
+for i in range(size):
+	heights.append(3+i)
+
+tot = 4
+i = 2
+while i < size:
+	if i > 1 and (i+1) % 2:
+		tot += 2
+	sup_stars.append(tot)
+	i += 1
+
+def get_line_len(floor, line):
+	tot = 0;
+	if floor > 0:
+		tot += get_line_len(floor - 1, heights[floor - 1] - 1 )
+		tot += 2 * (line+1) #Normal width increment
+		tot += sup_stars[floor]
+		return tot
+	else:
+		return 3 + 2 * line
+
+floor = 0;
+maxlen = get_line_len(size-1, heights[size-1])
+center = (maxlen - 1) / 2
+while floor < size:
+	line = 0;
+	while line < heights[floor]:
+		linelen = get_line_len(floor, line)
+		ltoprint = ""
+		begin = center - (linelen - 1) / 2
+		c = 0
+		while c < maxlen:
+			if c < begin:
+				ltoprint += ' '
+			if c == begin:
+				ltoprint += '/'
+			if c > begin and c < begin + linelen - 1:
+				ltoprint += '*'
+			if c == begin + linelen - 1:
+				ltoprint += '\\'
+			c += 1
+		print ltoprint
+		line += 1
+	floor += 1
